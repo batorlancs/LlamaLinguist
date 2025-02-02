220303177deccddf21894de75bff33da2fdde905
Gergely Bator <batorgergely3@gmail.com>
2025-02-02 18:15:19 +0100

    feat: new theme

--- frontend/src/features/auth/components/SkeletonImageCard.tsx
+++ frontend/src/features/auth/components/SkeletonImageCard.tsx
@@ -3,8 +3,8 @@ import { Skeleton } from "@/components/ui/skeleton";
 export function SkeletonImageCard() {
     return (
         <div className="flex flex-col items-center">
-            <Skeleton className="h-24 w-24" />
-            <Skeleton className="h-5 w-12 mt-2" />
+            <Skeleton className="h-24 w-24 bg-muted" />
+            <Skeleton className="h-5 w-12 mt-2 bg-muted" />
         </div>
     );
 }
